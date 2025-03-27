403b1f459b50a3a8e482c125e0d6fd2a5cd59d8e
RickISO <A01199137@tec.mx>
2025-03-27 14:19:21 -0600

    Ya compila :D

--- STAND_de_pruebas(eltroyano).ino
+++ STAND_de_pruebas(eltroyano).ino
@@ -15,7 +15,7 @@
 static String fileName = "";  /* Store file name static so after first change it wont be renamed again and 
 again and for the function to be called multiple times */
 static String firstTIMEcheck = ""; //to check if its the first time writing the SD down and put collum values and titles
-static int uint8_t Contador = 1 //contador para cuantos prints en la terminal
+static uint8_t Contador = 1; //contador para cuantos prints en la terminal
 
 BluetoothSerial SerialBT;
 
@@ -30,7 +30,7 @@ uint8_t Safe_Pin = 15;
 const uint8_t LOADCELL_DOUT_PIN = 32;  //Data pin for hx711
 const uint8_t LOADCELL_SCK_PIN = 33;   //sck pin for hx711
 //cambio(presion)
-const uint8_t PRESSURE_SENSOR_PIN = xx; //falta definir pin
+const uint8_t PRESSURE_SENSOR_PIN = 34; //falta definir pin
 
 
 
@@ -262,7 +262,7 @@ void launchMode(){
 void dataStore(){
   //SD card must me < 32 gb and formated FAT32
   
-  void dataStore() {
+  
   if (fileName == "") {  // Ask for file name only once due to the if and the static string
     SerialBT.print("Enter file name: ");
     while (!SerialBT.available()) {}  /* When a input is added a \n will be placed which will set a limit to the inputed "read" code
@@ -274,16 +274,20 @@ void dataStore(){
 
   myFile = SD.open(fileName, FILE_APPEND); //part of code that just writes down info (SHOULDNT BE TOUCHED BY ME SINCE I DONT FULLY UNDERSTAND IT)
   if (firstTIMEcheck=="") {
-    Serial.println("Force   Pressure    Time");
-    SerialBT.println("Force   Pressure    Time");
+    Serial.println("First thing printed");
+    SerialBT.println("First thing printed");
+    myFile.print("Force,Pressure,Time");
   }
   if (myFile) {
-    MATRIX data = [ForceValue, PressureValue, instance];
+    
+    
+    String data = String(ForceValue)+","+String(PressureValue)+","+String(instance);
     myFile.print(data);
+  
     //CHANGE TO A MATRIX WITH A TITLE AS A COLUMN THAT STATES WHAT NAME OF THE COLUMN the colum can be made with a static value as the previous
     
 /*  The snipet in coment should be more effcient as a writer, since it writes in binaries but due to knowledge constraints 
-on bytes the first part in the argument makes the arduino string into a c++ string since it's the only one that works 
+on bytes the first part in the argument makes the arduino String into a c++ string since it's the only one that works 
 for the .write and the min(31, data.lenght())) basically ensures that if more than 31 bytes are writen for only the lenght of 31 bytes to be accepted,
 however, I am not sure if every character means 1 byte therefore I am hesitant in using this however it should be more optimised*/
     //myFile.write(data.c_str(), min(31, data.length()));  // Write max 31 bytes (Normally the last byte isnt a full byte)
@@ -301,12 +305,13 @@ void ForceMeasure(){
 }
 
 
+
 //cambiar para mejor estructura (agregar el string del fuerza:, Tiempo:, Presion:)
 void dataTransfer(){
   
-  SerialBT.print(Contador + ". Fuerza: " + string(ForceValue) + ", Pression: "+ string(PressureValue) + "tiempo: "+ string(nstance));
-  Serial.print(Contador + ". Fuerza: " + string(ForceValue) + ", Pression: "+ string(PressureValue) + "tiempo: "+ string(nstance));
-  Contador=++
+  SerialBT.print(String(Contador) + ". Fuerza: " + String(ForceValue) + ", Pression: "+ String(PressureValue) + "tiempo: "+ String(instance));
+  Serial.print(String(Contador) + ". Fuerza: " + String(ForceValue) + ", Pression: "+ String(PressureValue) + "tiempo: "+ String(instance));
+  Contador++;
 }
 float readPressureSensor() {
   int sensorValue = analogRead(PRESSURE_SENSOR_PIN);
